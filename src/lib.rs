078ffb83086e9ae9c9d42c63a9aaf685bfa94a5a
unic0rn9k <unic0rn9k@protonmail.com>
2023-04-15 16:02:59 +0200

    Github CI badge

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! A library for automatic differentiation.
+//! A library for automatic differentiation. [![Rust](https://github.com/unic0rn9k/autodiff/actions/workflows/rust.yml/badge.svg)](https://github.com/unic0rn9k/autodiff/actions/workflows/rust.yml)
 //!
 //! # TODO
 //! - [ ] separate `Mul` and `MatMul`
@@ -6,6 +6,15 @@
 //! - [ ] `exp` operator
 //! - [ ] `sum` operator
 //! - [ ] `div` operator
+//!
+//! ## softmax
+//! sum = exp(x_0) + exp(x_1) + ... + exp(x_n)
+//! softmax(x_n) = exp(x_n) / sum
+//!
+//! softmax'(x_n) = softmax(x_n) * (1 - softmax(x_n))
+//!
+//! f(x)/g(x) -> (f'(x)g(x) - f(x)g'(x)) / g(x)^2
+//! exp(x) / sum -> (exp(x) * sum - exp(x) * exp(x)) / sum^2
 
 #![feature(test, array_zip)]
 
